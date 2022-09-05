bac1b9869e70aa12fc442247017017a084db2069
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-09-05 19:45:23 -0300

    Add logger

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ require('dotenv').config({ path: './config/config.env' });
 const { connectDB } = require('./config/db');
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 const PORT = process.env.PORT || 5000;
 const NODE_ENV = process.env.NODE_ENV;
 
 connectDB();
 
-app.get('/', (req, res) => {
-  res.json({ message: 'hello' });
-});
+// Logger for development
+if (NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.listen(PORT, () =>
   console.log(`Server running in ${NODE_ENV} mode on port: ${PORT}`)
--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB connected: ${conn?.connection.host}`);
+    console.log(`MongoDB connected ${conn?.connection.host}`);
   } catch (error) {
     console.log(error);
     process.exit(1);
