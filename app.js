dc93a35edc2b6b37f55dbeacb0fd0b821dedebf1
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-09-05 16:23:05 -0300

    Connect database

---
+++ app.js
@@ -0,0 +1,17 @@
+require('dotenv').config({ path: './config/config.env' });
+
+const { connectDB } = require('./config/db');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+const NODE_ENV = process.env.NODE_ENV;
+
+connectDB();
+
+app.get('/', (req, res) => {
+  res.json({ message: 'hello' });
+});
+
+app.listen(PORT, () =>
+  console.log(`Server running in ${NODE_ENV} mode on port: ${PORT}`)
+);
