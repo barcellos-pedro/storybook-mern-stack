65408ab19c9eaeaea69ec7ab0abd8e5ff3f03c95
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-09-05 20:16:46 -0300

    Add Template engine & layouts

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ const { connectDB } = require('./config/db');
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const { engine } = require('express-handlebars');
 const PORT = process.env.PORT || 5000;
 const NODE_ENV = process.env.NODE_ENV;
 
 connectDB();
+
+app.engine('.hbs', engine({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
+app.set('views', './views');
 
 // Logger for development
 if (NODE_ENV === 'development') {
