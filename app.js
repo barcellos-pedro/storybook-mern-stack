39f6256b395b3313f57f48ebcc300cef78275b5a
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-09-06 21:05:00 -0300

    Passport auth & Sessions config

--- app.js
+++ app.js
@@ -5,11 +5,28 @@ const { engine } = require('express-handlebars');
 const path = require('path');
 const errorHandler = require('./middlewares/error');
 const indexRoutes = require('./routes/index');
+const passport = require('passport');
+const session = require('express-session');
 const morgan = require('morgan');
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const NODE_ENV = process.env.NODE_ENV;
+
+// Sessions config
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+// Passport config
+const passportAuth = require('./config/passport-auth');
+passportAuth(passport);
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Connect to Database
 connectDB();
