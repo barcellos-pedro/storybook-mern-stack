a0891036063a764123aa1447365ac47de838580c
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-09-07 13:52:25 -0300

    Add auth routes

---
+++ routes/auth.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const passport = require('passport');
+const router = express.Router();
+const authController = require('../controllers/auth');
+
+router.get(
+  '/google',
+  passport.authenticate('google', {
+    scope: ['profile'],
+  })
+);
+
+router.get(
+  '/google/callback',
+  passport.authenticate('google', { failureRedirect: '/' }),
+  authController.auth
+);
+
+module.exports = router;
