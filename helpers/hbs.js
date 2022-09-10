2e0e0f08f4f0d3046a20cf462b0e618fd35ee180
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-09-10 20:15:56 -0300

    Create truncate and stripTags

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -1,3 +1,26 @@
+/**
+ * Formate date to lacale
+ *
+ * Example: 9/10/2022, 6:56:32 PM (pt-BR)
+ */
 const formatDate = (date) => new Date(date).toLocaleString();
 
-module.exports = { formatDate };
+/**
+ * Return shorter text
+ */
+const truncate = (text, size = 50) => {
+  if (text.length < size) {
+    return text;
+  }
+  const formattedText = text.substr(0, size);
+  return `${formattedText}...`;
+};
+
+/**
+ * Remove html tags from content
+ */
+const stripTags = (input) => {
+  return input.replace(/<(?:.|\n)*?>/gm, '');
+};
+
+module.exports = { formatDate, truncate, stripTags };
