9d4570997348c04cc9d0c8b1dbaa7fbbee819601
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-09-10 19:01:37 -0300

    Add create story method

--- controllers/stories.js
+++ controllers/stories.js
@@ -1,3 +1,5 @@
+const Story = require('../models/Story');
+
 /**
  * Create Story page
  * @route GET /stories/add
@@ -7,6 +9,21 @@ const add = async (req, res) => {
   res.render('stories/add');
 };
 
+/**
+ * Create Story
+ * @route POST /stories/add
+ * @access Private
+ */
+const create = async (req, res) => {
+  try {
+    await Story.create({ ...req.body, user: req.user._id });
+    res.redirect('/dashboard');
+  } catch (error) {
+    res.render('error/500');
+  }
+};
+
 module.exports = {
   add,
+  create,
 };
