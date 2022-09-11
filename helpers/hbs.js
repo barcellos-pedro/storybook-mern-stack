6bd345a4ecfbf759118755a35b1f2ac6171471cd
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-09-10 21:33:14 -0300

    add editIcon helper

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -22,4 +22,13 @@ const stripTags = (input) => {
   return input.replace(/<(?:.|\n)*?>/gm, '');
 };
 
-module.exports = { formatDate, truncate, stripTags };
+const editIcon = (storyUser, loggedUser, storyId, floating = true) => {
+  if (storyUser?._id.toString() !== loggedUser?._id.toString()) {
+    if (!floating) {
+      return `<a href="/stories/edit/${storyId}"><i class="fas fa-edit"></i></a>`;
+    }
+    return `<a href="/stories/edit/${storyId}" class="btn-floating halfway-fab blue"><i class="fas fa-edit fa-small"></i></a>`;
+  }
+};
+
+module.exports = { formatDate, truncate, stripTags, editIcon };
